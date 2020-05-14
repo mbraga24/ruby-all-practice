c6d9ffc4c0452c34fb3a6dfd4e2ef748033fe15b
github username <github email address>
2020-05-13 22:38:01 -0400

    Relating Objects with 'belongs to' and 'has many'

--- basics-of-object-relationships-2.rb
+++ basics-of-object-relationships-2.rb
@@ -15,11 +15,37 @@ class Artist
     @name = name
     @songs = []
   end
+
+  def add_song(song)
+    @songs.push(song)
+  end
+
+  def songs
+    @songs
+  end
 end
 
-artist_1 = Artist.new("The Weeknd")
-track_1 = Song.new("Blinded By The Lights", "Pop Music")
-p track_1.name
-p track_1.genre
-track_1.artist = artist_1
-binding.pry
+# "Too late"
+# "Heartless"
+# "Snowchild"
+
+theWeeknd = Artist.new("The Weeknd")
+
+# ===========================================================
+# Call add_song method with an argument of a real song object:
+# -----------------------------------------------------------
+blinded = Song.new("Blinded By The Lights", "Pop Music")
+tooLate = Song.new("Too Late", "Alternative R&B")
+
+theWeeknd.add_song(blinded)
+theWeeknd.add_song(tooLate)
+
+# ========================================================
+# Collect all the genre.
+# --------------------------------------------------------
+all_genres = theWeeknd.songs.collect do |song|
+  song.genre
+end
+# p all_genres
+
+# binding.pry
