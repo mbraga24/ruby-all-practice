529b1c3b8285788a13031a5ac9c17fcdeabe1a50
github username <github email address>
2020-05-17 17:32:21 -0400

    Add #meals intance method to Customer and Waiter class

--- has-many-through-relationship.rb
+++ has-many-through-relationship.rb
@@ -10,7 +10,7 @@ class Waiter
   end
 
   # (customer, waiter, meal, tip=0)
-  def self.new_meal(customer, meal, tip)
+  def new_meal(customer, meal, tip)
     Meal.new(customer, self, meal, tip)
   end
 
@@ -31,8 +31,12 @@ class Customer
   end
 
   # (customer, waiter, meal, tip=0)
-  def self.new_meal(waiter, meal, tip)
+  def new_meal(waiter, meal, tip)
     Meal.new(self, waiter, meal, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.customer == self }
   end
 
   def self.all
@@ -58,3 +62,27 @@ class Meal
     @@meals
   end
 end 
+
+alexandra = Customer.new("Alexandra", 30)
+marcelo = Customer.new("Marcelo", 27)
+marlon = Customer.new("Marlon", 27)
+lauro = Waiter.new("Lauro", 2)
+parafina = Waiter.new("Parafina", 5)
+
+# CUSTOMER - # A Customer creates a Meal, passing in a Waiter instance
+marlon.new_meal(lauro, 50, 10) 
+marlon.new_meal(parafina, 20, 3) 
+marcelo.new_meal(parafina, 100, 40) 
+
+# WAITER - # A Waiter creates a Meal, passing in a Customer instance
+lauro.new_meal(alexandra, 300, 100) 
+lauro.new_meal(marlon, 30, 5)  
+parafina.new_meal(marcelo, 15, 3) 
+
+p alexandra.meals
+puts ""
+p marcelo.meals
+puts ""
+p marlon.meals
+puts "==========="
+p Meal.all.length
